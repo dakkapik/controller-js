18bc4bc329b41f99fe3b6f34ec00a7fbaa8c5cca
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-03-12 10:54:44 -0400

    gamepads

---
+++ interface.js
@@ -0,0 +1,15 @@
+let x = 0;
+let y = 0;
+
+// controllers[0].axes
+
+function setup() {
+    createCanvas(400, 400);
+}
+  
+function draw() {
+    fill(51)
+    background(220);
+    rect(x+(width*0.5), y+(height*0.5), 10,10,15);
+}
+  
