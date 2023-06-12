89672357603c491c4931239c9682184f2733b8a9
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-06-12 13:14:49 -0400

    more

--- socket.js
+++ socket.js
@@ -1,2 +1,7 @@
 //go to server 
-const socket = io("")
+const socket = io("http://192.168.2.11:3000")
+
+socket.on("connection", ()=> {
+    console.log("c")
+    socket.emit("ID", "js-controller")
+})
